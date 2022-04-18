ab337e3a5f7f2e81e85def5c54ef7121297c07c9
Marcus <mpollard33@gmail.com>
2022-04-18 12:57:00 -0600

    Init

--- config/routes2.rb
+++ config/routes2.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-#<<<<<<< HEAD
-#=======
+<<<<<<< HEAD
+=======
   resources :projects
-  root "projects#index"
-end
-#>>>>>>> f61b577f867ce58d69436a7b7a214f87e98c6cc9
+>>>>>>> f61b577f867ce58d69436a7b7a214f87e98c6cc9
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Defines the root path route ("/")
   # root "articles#index"
+<<<<<<< HEAD
+=======
+  
+      root "projects#index"
+
+  end
+>>>>>>> f61b577f867ce58d69436a7b7a214f87e98c6cc9
+end
